c00899a4ac35f7205824dd0559b7fbf44a8ac2e3
boarwell <34891695+boarwell@users.noreply.github.com>
2022-03-24 19:11:06 +0900

    vitest

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,3 +1,4 @@
+import { test, expect } from "vitest";
 import { hira2kata, hira2kataAll, kata2hira, kata2hiraAll } from "./main.js";
 
 type Case = {
@@ -5,7 +6,7 @@ type Case = {
   expected: string;
 };
 
-const test_hira2kata = () => {
+test("hira2kata", () => {
   const cases: Case[] = [
     { input: "あ", expected: "ア" },
     { input: "を", expected: "ヲ" },
@@ -14,46 +15,24 @@ const test_hira2kata = () => {
     { input: "愛", expected: "慻" },
   ];
 
-  const failedCases = cases.filter(
-    ({ input, expected }) => hira2kata(input) !== expected
-  );
-
-  if (failedCases.length <= 0) {
-    return;
+  for (const { expected, input } of cases) {
+    expect(hira2kata(input)).toBe(expected);
   }
+});
 
-  console.error("[Failed]: hira2kata()");
-  for (const { input, expected } of failedCases) {
-    console.error(
-      `input: ${input}, expected ${expected}, but got ${hira2kata(input)}`
-    );
-  }
-};
-
-const test_hira2kataAll = () => {
+test("hira2kataAll", () => {
   const cases: Case[] = [
     { input: "こんにちは", expected: "コンニチハ" },
     { input: "今日は", expected: "今日ハ" },
     { input: "GoとRust", expected: "GoトRust" },
   ];
 
-  const failedCases = cases.filter(
-    ({ input, expected }) => hira2kataAll(input) !== expected
-  );
-
-  if (failedCases.length <= 0) {
-    return;
+  for (const { expected, input } of cases) {
+    expect(hira2kataAll(input)).toBe(expected);
   }
+});
 
-  console.error("[Failed]: hira2kata()");
-  for (const { input, expected } of failedCases) {
-    console.error(
-      `input: ${input}, expected ${expected}, but got ${hira2kataAll(input)}`
-    );
-  }
-};
-
-const test_kata2hira = () => {
+test("kata2hira", () => {
   const cases: Case[] = [
     { input: "ア", expected: "あ" },
     { input: "ヲ", expected: "を" },
@@ -62,50 +41,19 @@ const test_kata2hira = () => {
     { input: "慻", expected: "愛" },
   ];
 
-  const failedCases = cases.filter(
-    ({ input, expected }) => kata2hira(input) !== expected
-  );
-
-  if (failedCases.length <= 0) {
-    return;
+  for (const { expected, input } of cases) {
+    expect(kata2hira(input)).toBe(expected);
   }
+});
 
-  console.error("[Failed]: kata2hira()");
-  for (const { input, expected } of failedCases) {
-    console.error(
-      `input: ${input}, expected ${expected}, but got ${kata2hira(input)}`
-    );
-  }
-};
-
-const test_kata2hiraAll = () => {
+test("kata2hiraAll", () => {
   const cases: Case[] = [
     { input: "コンニチハ", expected: "こんにちは" },
     { input: "今日ハ", expected: "今日は" },
     { input: "GoトRust", expected: "GoとRust" },
   ];
 
-  const failedCases = cases.filter(
-    ({ input, expected }) => kata2hiraAll(input) !== expected
-  );
-
-  if (failedCases.length <= 0) {
-    return;
+  for (const { input, expected } of cases) {
+    expect(kata2hiraAll(input)).toBe(expected);
   }
-
-  console.error("[Failed]: kata2hiraAll()");
-  for (const { input, expected } of failedCases) {
-    console.error(
-      `input: ${input}, expected ${expected}, but got ${kata2hiraAll(input)}`
-    );
-  }
-};
-
-const main = () => {
-  test_hira2kata();
-  test_hira2kataAll();
-  test_kata2hira();
-  test_kata2hiraAll();
-};
-
-main();
+});
